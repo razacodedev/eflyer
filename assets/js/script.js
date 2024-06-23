9445cbc394322d77d8a8275e478d64803b3e9e52
Ahmed Raza <124212183+razacodedev@users.noreply.github.com>
2024-06-24 03:31:04 +0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// function showSlide(slideIndex) {
+//     // Update currentSlide (already done in click handler)
+  
+//     // Remove "active" from all slides
+//     $('.card-section').removeClass('active');
+  
+//     // Add "active" to the current slide
+//     $('.card-section').eq(slideIndex).addClass('active');
+  
+//     // You can add any animation logic here
+//   }
+  
